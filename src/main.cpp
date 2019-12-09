9b2b4a5aeda50a28fc89c69a271e0f31f6dd8886
ramoncinp <ramon.parra@chipred.com>
2019-12-09 10:43:54 -0600

    Anexar relacion de estados de WiFi con sus numeros

--- src/main.cpp
+++ src/main.cpp
@@ -75,6 +75,16 @@ void handleLed()
   }
 }
 
+/*
+  WL_IDLE_STATUS      = 0
+  WL_NO_SSID_AVAIL    = 1
+  WL_SCAN_COMPLETED   = 2
+  WL_CONNECTED        = 3
+  WL_CONNECT_FAILED   = 4
+  WL_CONNECTION_LOST  = 5
+  WL_DISCONNECTED     = 6
+*/
+
 void handleWiFiConnection()
 {
   static wl_status_t mStatus = WL_IDLE_STATUS;
